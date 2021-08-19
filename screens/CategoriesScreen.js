164c13fd2165c04e435d3ae4f09d1a47a3739860
EAD\rnair63 <rnair63@dxc.com>
2021-08-19 15:04:32 +0530

    added dynamic navigation options

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,31 +1,77 @@
 import React from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  FlatList,
+  Text,
+  View,
+  TouchableOpacity,
+  Platform,
+} from "react-native";
+
+import Colors from "../constants/Colors";
+import { CATEGORIES } from "../data/dummy-data";
 
 function CategoriesScreen({ navigation: { navigate, push, goBack, replace } }) {
-  const { screen } = styles;
+  const { screen, gridItemStyle } = styles;
+
+  const renderGridItem = (itemData) => {
+    return (
+      <TouchableOpacity
+        style={gridItemStyle}
+        onPress={() => {
+          navigate({
+            routeName: "CategoryMeals",
+            params: {
+              categoryId: itemData.item.id,
+            },
+          });
+        }}
+      >
+        <View>
+          <Text>{itemData.item.title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
   return (
-    <View style={screen}>
-      <Text>Categories screen</Text>
-      <Button
-        title="Go to meals"
-        onPress={() => {
-          navigate("CategoryMeals");
-        }}
-      />
-      <Button
-        title="Go to meals"
-        onPress={() => {
-          replace("CategoryMeals");
-        }}
-      />
-    </View>
+    <FlatList numColumns={2} data={CATEGORIES} renderItem={renderGridItem} />
+    // <View style={screen}>
+    //   <Text>Categories screen</Text>
+    //   <Button
+    //     title="Go to meals"
+    //     onPress={() => {
+    //       navigate("CategoryMeals");
+    //     }}
+    //   />
+    //   <Button
+    //     title="Go to meals"
+    //     onPress={() => {
+    //       replace("CategoryMeals");
+    //     }}
+    //   />
+    // </View>
   );
 }
 
+CategoriesScreen.navigationOptions = {
+  title: "Meal Categories",
+  headerStyle: {
+    backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
+  },
+  headerTintColor: Platform.OS === "android" ? "white" : Colors.primaryColor,
+};
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: "center",
+    alignItems: "center",
+  },
+  gridItemStyle: {
+    flex: 1,
+    margin: 15,
+    justifyContent: "center",
+    height: 150,
     alignItems: "center",
   },
 });
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, Platform, StyleSheet, Text, View } from "react-native";
+import Colors from "../constants/Colors";
 
-function CategoryMealsScreen({ navigation: { navigate, goBack } }) {
+import { CATEGORIES } from "../data/dummy-data";
+
+function CategoryMealsScreen({ navigation: { navigate, goBack, getParam } }) {
+  const catId = getParam("categoryId");
   const { screen } = styles;
+
+  const selectedCategory = CATEGORIES.find((category) => category.id === catId);
   return (
     <View style={screen}>
-      <Text>Categories meals screen</Text>
+      <Text>{selectedCategory.title}</Text>
       <Button
         title="Go to meal details"
         onPress={() => {
@@ -21,6 +27,19 @@ function CategoryMealsScreen({ navigation: { navigate, goBack } }) {
     </View>
   );
 }
+
+CategoryMealsScreen.navigationOptions = ({ navigation: { getParam } }) => {
+  const catId = getParam("categoryId");
+  const selectedCategory = CATEGORIES.find((category) => category.id === catId);
+
+  return {
+    title: selectedCategory.title,
+    headerStyle: {
+      backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
+    },
+    headerTintColor: Platform.OS === "android" ? "white" : Colors.primaryColor,
+  };
+};
 
 const styles = StyleSheet.create({
   screen: {
